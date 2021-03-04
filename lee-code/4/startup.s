90237dd403b4632b5ca55017372ecd6c7aaafc03
harrolee <lharrold@scu.edu>
2021-03-03 21:20:16 -0800

    end of 4

--- lee-code/4/startup.s
+++ lee-code/4/startup.s
@@ -1,6 +1,6 @@
 /* defines for processor modes */
 .equ MODE_FIQ, 0X11
-.equ MODE_IRG, 0x12
+.equ MODE_IRQ, 0x12
 .equ MODE_SVC, 0x13
 
 .section .vector_table, "x"
@@ -40,3 +40,51 @@ fiq_loop:
 	strlt r0, [r1], #4
 	// if the value in r1 was less than the value in sp, run this loop again
 	blt fiq_loop    
+
+	/* IRQ stack */
+	msr cpsr_c, MODE_IRQ
+	ldr r1, =_irq_stack_start
+	ldr sp, =_irq_stack_end
+
+irq_loop:
+	cmp r1, sp
+	strlt r0, [r1], #4
+	blt irq_loop
+
+
+	/* Supervisor mode */
+	msr cpsr_c, MODE_SVC
+	ldr r1, =_stack_start
+	ldr sp, =_stack_end
+
+stack_loop:
+	cmp r1, sp
+	strlt r0, [r1], #4
+	blt stack_loop
+
+	/* Start copying data */
+	ldr r0, =_text_end
+	ldr r1, =_data_start
+	ldr r2, =_data_end
+
+data_loop:
+	cmp r1, r2
+	ldrlt r3, [r0], #4
+	strlt r3, [r1], #4
+	blt data_loop
+
+	/* Initialize .bss */
+	mov r0, #0
+	ldr r1, =_bss_start
+	ldr r2, =_bss_end
+
+bss_loop:
+	cmp r1, r2
+	strlt r0, [r1], #4
+	blt bss_loop
+
+	bl main
+	b Abort_Exception
+
+Abort_Exception:
+	swi 0xFF
